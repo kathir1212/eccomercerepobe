d6497cca7abae3760ce14f05b560b947032d75a6
kathir1212 <kathirvel082@gmail.com>
2025-07-29 20:13:14 +0530

    deploy test

--- controller/userController.js
+++ controller/userController.js
@@ -68,8 +68,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'development',
-      sameSite: process.env.NODE_ENV === 'development' ? 'none' : 'strict',
+      secure: true,
+      sameSite:  'none' ,
    
     
     
